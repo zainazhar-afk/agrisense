f3be8fb271b08d9b24d6dd21bcdd3ab108174492
ZAINAZHAR303 <zainazhar303@gmail.com>
2026-02-26 14:44:37 +0500

    feat: Export ResultDisplay component in disease detection module

--- frontend/src/features/disease-detection/components/index.js
+++ frontend/src/features/disease-detection/components/index.js
@@ -1,4 +1,5 @@
 export { ImageUploadSection } from "./ImageUpload";
 export { HowItWorks } from "./HowItWorks";
 export { ResultPreview } from "./ResultPreview";
+export { ResultDisplay } from "./ResultDisplay";
 export { TrustFeatures } from "./TrustFeatures";
